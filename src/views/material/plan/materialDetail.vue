cfc54da989831de6c12e1f1523d3893eec874e5e
wangyouzheng <353735348@qq.com>
2024-03-18 13:36:57 +0800

    物料计划

--- src/views/material/plan/materialDetail.vue
+++ src/views/material/plan/materialDetail.vue
@@ -2,58 +2,107 @@
   要货计划，物料详情
 -->
 <template>
-<!--  <div>-->
-  <el-table v-loading="loading" :data="tableData">
-    <el-table-column type="selection" width="55" align="center" />
-    <el-table-column label="" align="center" prop="id" />
-<!--    <el-table-column label="项目名称" align="center" prop="projectName" />
-    <el-table-column label="项目id" align="center" prop="projectId" />
-    <el-table-column label="项目经理" align="center" prop="projectManagerName" />
-    <el-table-column label="项目经理id" align="center" prop="projectManagerId" />
-    <el-table-column label="要货计划id" align="center" prop="planId" />
-    <el-table-column label="计划名称" align="center" prop="planName" />
-    <el-table-column label="物料名称" align="center" prop="materialName" />
-    <el-table-column label="物料规格" align="center" prop="materialSpec" />
-    <el-table-column label="物料单位" align="center" prop="materialUnit" />
-    <el-table-column label="物料数量" align="center" prop="materialNumber" />
-    <el-table-column label="最晚进场时间" align="center" prop="entryDeadlineDate" width="180">
-      <template #default="scope">
-        <span>{{ parseTime(scope.row.entryDeadlineDate, '{y}-{m}-{d}') }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column label="备注" align="center" prop="remark" />
-    <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
-      <template #default="scope">
-        <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['system:detail:edit']">修改</el-button>
-        <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['system:detail:remove']">删除</el-button>
-      </template>
-    </el-table-column>-->
-  </el-table>
+  <div>
+    <el-table ref="detailTable" :max-height="tableHeight" :data="tableData">
+      <el-table-column type="selection" width="55" align="center"/>
+<!--      <el-table-column label="序号" type="index" width="50" align="center">
+        <template #default="scope">
+          <span>{{(queryParams.pageNum - 1) * queryParams.pageSize + scope.$index + 1}}</span>
+        </template>
+      </el-table-column>-->
+<!--      <el-table-column label="" align="center" prop="id"/>-->
+      <el-table-column label="项目名称" align="center" prop="projectName"/>
+<!--      <el-table-column label="项目id" align="center" prop="projectId"/>-->
+      <el-table-column label="项目经理" align="center" prop="projectManagerName"/>
+<!--      <el-table-column label="项目经理id" align="center" prop="projectManagerId"/>-->
+<!--      <el-table-column label="要货计划id" align="center" prop="planId"/>-->
+      <el-table-column label="计划名称" align="center" prop="planName"/>
+      <el-table-column width="220" label="物料名称" align="center" prop="materialName">
+        <template #default="scope">
+          <el-select value-key="userId" v-model="scope.row.materialName" style="width:100%">
+            <el-option
+                v-for="item in materialOptions"
+                :key="item.id"
+                :label="item.materialName"
+                :value="item"
+                :disabled="item.status == 1"
+            >
+              <span style="float: left">{{ item.materialName + item.materialSpec + item.materialUnitName}}}</span>
+              <span
+                  style="
+          float: right;
+          color: var(--el-text-color-secondary);
+          font-size: 13px;"
+              >{{ item.value }}</span>
+            </el-option>
+          </el-select>
+        </template>
+      </el-table-column>
 
-<!--  <el-button class="mt-4" style="width: 100%" @click="onAddItem">Add Item</el-button>-->
-<!--  </div>-->
+      <el-table-column label="物料规格" align="center" prop="materialSpec">
+        <template #default="scope">
+          {{scope.row.materialName ? scope.row.materialName.materialSpec : ''}}
+        </template>
+      </el-table-column>
+      <el-table-column label="物料单位" align="center" prop="materialUnit">
+        <template #default="scope">
+          {{scope.row.materialName ? scope.row.materialName.materialUnitName : ''}}
+        </template>
+      </el-table-column>
+      <el-table-column label="物料数量" align="center" prop="materialNumber" width="400">
+        <template #default="scope">
+          <el-input-number v-model="scope.row.materialNumber" :min="1" @change="handleChange" />
+        </template>
+      </el-table-column>
+      <el-table-column label="最晚进场时间" align="center" prop="entryDeadlineDate" width="110">
+        <template #default="scope">
+          <span>{{ parseTime(scope.row.entryDeadlineDate, '{y}-{m}-{d}') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="备注" align="center" prop="remark"/>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template #default="scope">
+          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
+                     v-hasPermi="['system:detail:edit']">
+            保存
+          </el-button>
+          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
+                     v-hasPermi="['system:detail:remove']">删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-button class="mt-4" style="width: 100%" @click="onAddItem">新增一行</el-button>
+  </div>
 </template>
 
-<script setup name="materialDetail">
+<script setup>
+import {listProjectInfo} from "@/api/base/info.js";
+import {listMaterial} from '@/api/base/material.js';
 
-const tableData = ref([])
+const tableData = ref([{"id": 1}])
+const tableHeight = ref(document.documentElement.scrollHeight - 245 + "px");
 
 const loading = ref(false);
 
+// 所属物料信息
+const materialOptions = ref([]);
+
 const onAddItem = () => {
   tableData.value.push({
-    date: dayjs(now).format('YYYY-MM-DD'),
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
+    // date: dayjs(now).format('YYYY-MM-DD'),
   })
 }
 
+function init() {
+  listMaterial({
+    pageNum: 1,
+    pageSize: 1000}).then(response => {
+    materialOptions.value = response.rows;
+  });
+}
+init();
+
+
 </script>
-
-
-<style scoped lang="scss">
-
-</style>
--- src/views/material/plan/planDetail.vue
+++ src/views/material/plan/planDetail.vue
@@ -2,32 +2,35 @@
   要货计划详情页容器
 -->
 <template>
-  <el-card>
-    <el-tabs v-model="activeName">
-      <el-tab-pane label="基本信息" name="basePlan">
-        <base-info-form ref="baseInfo" :info="info" :projectOptions="projectOptions" />
-      </el-tab-pane>
-      <el-tab-pane label="计划详情" name="materialDetail">
-        <material-detail ref="materialDetail"/>
-      </el-tab-pane>
-    </el-tabs>
-  </el-card>
-
-  <el-form label-width="100px">
-    <div style="text-align: center;margin-left:-100px;margin-top:10px;">
-      <el-button type="primary" @click="submitForm()">提交</el-button>
-      <el-button @click="close()">返回</el-button>
-    </div>
-  </el-form>
+    <el-card>
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="基本信息" name="baseTab">
+          <base-info-form ref="baseInfo" :info="info" :projectOptions="projectOptions" />
+        </el-tab-pane>
+        <el-tab-pane label="计划详情" name="materialDetailTab">
+          <material-detail ref="materialDetails"/>
+        </el-tab-pane>
+      </el-tabs>
+      <el-form label-width="100px">
+        <div style="text-align: center;margin-left:-100px;margin-top:10px;">
+          <el-button type="primary" @click="submitForm()">提报计划</el-button>
+          <el-button type="primary" @click="submitForm()">保存</el-button>
+          <el-button @click="close()">返回</el-button>
+        </div>
+      </el-form>
+    </el-card>
 </template>
 
 <script setup name="PlanDetail">
 import baseInfoForm from "./baseInfoForm.vue";
 import materialDetail from './materialDetail.vue'
-
 import {listProjectInfo} from "@/api/base/info.js";
 
-const activeName = ref("basePlan");
+
+const { proxy } = getCurrentInstance();
+const route = useRoute();
+
+const activeName = ref("baseTab");
 const info = ref({});
 const projectOptions = ref([])
 
@@ -37,6 +40,15 @@ function init() {
     pageSize: 100}).then(response => {
     projectOptions.value = response.rows;
   });
+}
+
+function submitForm() {
+
+}
+
+function close() {
+  const obj = { path: "/material/material/plan", query: { t: Date.now(), pageNum: route.query.pageNum } };
+  proxy.$tab.closeOpenPage(obj);
 }
 
 init();
